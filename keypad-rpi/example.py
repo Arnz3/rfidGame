5932cce5e81b457529b23bd5123a936c46f32c45
Arnz3 <cuyversarno@gmail.com>
2022-02-04 13:45:49 +0100

    keypad example

---
+++ keypad-rpi/example.py
@@ -0,0 +1,14 @@
+import time
+import RPi.GPIO as GPIO
+from keypad import keypad
+
+GPIO.setwarnings(False)
+while True:
+	if __name__ == "__main__":
+		kp = keypad(columnCount=3)
+
+		digit = None
+		while digit == None:
+			digit = kp.getKey()
+		print digit
+		time.sleep(0.5)
