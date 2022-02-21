8febe40383f077db1d611e5650e0875c20206fea
Dré Vanhoucke <dre.vanhoucke@student.hogent.be>
2022-02-21 12:25:47 +0100

    write methode aangepast

--- database.py
+++ database.py
@@ -7,15 +7,22 @@ config = {'user': 'root', 'password': 'ArnoSuckt', 'host' : '10.10.12.50', 'data
 conn = mysql.connector.connect(**config)
 cursor = conn.cursor()
 
-def write(data, table):
-	sql = "INSERT INTO " + table + " VALUES (" + data + ");"
-	cursor.execute(sql)
-	conn.commit()
-
 def read(table):
 	sql = "SELECT * FROM "+ table + ";"
 	cursor.execute(sql)
 	return cursor.fetchall()
 
-write('420', 'arno')
-print(read('arno'))
+def write(naam, bedrag, code):
+	cursor.execute("SELECT code FROM users WHERE naam = " + naam + ";")
+	if (cursor.fetchall() == code):
+		cursor.execute("SELECT totaal FROM users WHERE naam = " + naam + ";")
+		huidigTotaal = cursor.fetchall()
+
+		nieuwTotaal = huidigTotaal + bedrag
+		cursor.execute("UPDATE users SET totaal = " + nieuwTotaal + " WHERE naam = " + naam + ";")
+		cursor.commit()
+
+		return "Niet totaal van " + naam + " is: " + nieuwTotaal
+	
+	else:
+		return "Foute code!"
