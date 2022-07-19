58b31bf117df390319a199045a76f1f34856ca04
Arnz3 <49102865+Arnz3@users.noreply.github.com>
2022-07-19 22:36:59 +0200

    Update database.py

--- database.py
+++ database.py
@@ -7,32 +7,28 @@ config = {'user': 'vanhouckedre_bechiro', 'password': 'ArnoSuckt', 'host' : 'van
 conn = mysql.connector.connect(**config)
 cursor = conn.cursor()
 
-def getNaam(nummer):
-	cursor.execute("SELECT naam FROM kaarten WHERE nummer = " + nummer + ";")
-	return cursor.fetchall()
-
-def read(naam, code):
-	cursor.execute("SELECT code FROM users WHERE naam = " + naam + ";")
+def read(nummer, code):
+	cursor.execute("SELECT code FROM rfid WHERE naam = " + nummer + ";")
 	if (cursor.fetchall() == code):
-		cursor.execute("SELECT totaal FROM users WHERE naam = " + naam + ";")
+		cursor.execute("SELECT bedrag FROM rfid WHERE naam = " + nummer + ";")
 		huidigTotaal = cursor.fetchall()
 
-		return "totaal van " + naam + " is: " + huidigTotaal
+		return "totaal van " + nummer + " is: " + huidigTotaal
 
 	else:
 		return "Foute code!"
 
-def write(naam, bedrag, code):
-	cursor.execute("SELECT code FROM users WHERE naam = " + naam + ";")
+def write(nummer, bedrag, code):
+	cursor.execute("SELECT code FROM rfid WHERE nummer = " + nummer + ";")
 	if (cursor.fetchall() == code):
-		cursor.execute("SELECT totaal FROM users WHERE naam = " + naam + ";")
+		cursor.execute("SELECT bedrag FROM rfid WHERE nummer = " + nummer + ";")
 		huidigTotaal = cursor.fetchall()
 
 		nieuwTotaal = huidigTotaal + bedrag
-		cursor.execute("UPDATE users SET totaal = " + nieuwTotaal + " WHERE naam = " + naam + ";")
+		cursor.execute("UPDATE rfid SET bedrag = " + nieuwTotaal + " WHERE nummer = " + nummer + ";")
 		cursor.commit()
 
-		return "Nieuw totaal van " + naam + " is: " + nieuwTotaal
+		return "Nieuw totaal van " + nummer + " is: " + nieuwTotaal
 	
 	else:
 		return "Foute code!"
