34a992cbea1469e5cbb72c7b385f4a02f2075e2f
Arnz3 <cuyversarno@gmail.com>
2022-07-24 04:02:07 +0200

    local mysql server

--- database.py
+++ database.py
@@ -2,8 +2,8 @@
 
 import mysql.connector
 
-config = {'user': 'vanhouckedre_bechiro', 'password': 'DreStinkt', 'host' : 'vanhouckedre.be.mysql', 'database': 'vanhouckedre_bechiro'}
-# config = {'user': 'arno', 'password': 'test1234', 'host' : '192.168.0.125', 'database': 'rfidgame'}
+# config = {'user': 'vanhouckedre_bechiro', 'password': 'DreStinkt', 'host' : 'vanhouckedre.be.mysql', 'database': 'vanhouckedre_bechiro'}
+config = {'user': 'arno', 'password': 'test1234', 'host' : '192.168.0.125', 'database': 'rfidgame'}
 
 conn = mysql.connector.connect(**config)
 cursor = conn.cursor()
